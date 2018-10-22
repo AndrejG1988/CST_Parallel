eee843d606229a58b9f16532072fab89652eef2e
AndrejG1988 <43929034+AndrejG1988@users.noreply.github.com>
2018-10-22 23:39:45 +0200

    Add files via upload

--- CST_PVS/CST_parallel.c
+++ CST_PVS/CST_parallel.c
@@ -1,39 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 #pragma warning(disable : 4996)
+
+
+
 void dateiEinlesen();
+char randomStringNumber();
+
 int main(int argc, char **argv) {
+
+
+
 	dateiEinlesen();
 	//Testvariables
 	char str1[] = "123456";
-	char str2[] = "123654";
+	char str2[] = "122456";
+	int laengegesamt = 6;
+	randomStringNumber(laengegesamt);
+
 	hammingDistanz(str1, str2);
 	system("pause");
 	return 0;
 }
+
 // einlesen der Informationen aus der Quelle "strings.txt" Datei
 //inhaltQuelle dateiLesen() {
 void dateiEinlesen() {
+
 	FILE *quelle;
+
 	/* Bitte Pfad und Dateinamen anpassen */
 	quelle = fopen("strings.txt", "r");
 	size_t laenge = 255;
+
 	int stringGroesse = 0;
 	int stringLaenge = 0;
 	void** inhaltQuelle;
 	// Speicher reservieren
 	char *zeile = malloc(sizeof(char)*laenge);
-	//prÃ¼fe ob die datei auch existiert
+
+	//prüfe ob die datei auch existiert
 	if (NULL == quelle) {
-		printf("Konnte Datei \"test.txt\" nicht Ã¶ffnen!\n");
+		printf("Konnte Datei \"test.txt\" nicht öffnen!\n");
 		return 1;
 	}
+
 	//erste Zeile der Text datei auslesen und in eine Variabele speichern, mit fgets(ziel, n menge, quelle).
 	if (fgets(zeile, laenge, quelle) != NULL) {
+
 		stringGroesse = atoi(zeile);
 		printf("Groesse: %s\n", zeile);
 	}
+
 	//zweite Zeile der Text datei auslesen und in eine Variabele speichern, mit fgets(ziel, n menge, quelle).
 	if (fgets(zeile, laenge, quelle) != NULL) {
 		stringLaenge = atoi(zeile);
@@ -51,13 +71,16 @@ void dateiEinlesen() {
 		count++;
 	}
 	printf("\n");
+
 	free(zeile);
 }
+
 int hammingDistanz(char *str1, char *str2)
 {
 	int i = 0, count = 0;
 	while (str1[i] != NULL)
 	{
+
 		if (str1[i] != str2[i])
 		{
 			count++;
@@ -65,6 +88,8 @@ int hammingDistanz(char *str1, char *str2)
 		i++;
 	}
 	printf("Die Hamming Distanz ist: %d \n", count);
+
 	return count;
+
 }
 
